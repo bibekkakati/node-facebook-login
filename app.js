a21c4bbd5970b1d5d994cb606a3c017021cc3730
Bibek <34815829+bibekkakati@users.noreply.github.com>
2019-10-15 03:41:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.set("view engine", "ejs");
 app.use(require("morgan")("combined"));
 app.use(require("cookie-parser")());
 app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(passport.initialize());
 app.use(
   require("express-session")({
     secret: "node app",
